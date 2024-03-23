0683db69d32a7a8991f3da23255504ce740dde83
cy6erninja <cy6erninja@gmail.com>
2024-03-23 15:42:13 +0100

    Moved the first task to its module.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
-use std::fs::{read};
-use std::path::Path;
-
 fn main() {
-    let puzzle_input_filepath = Path::new("assets/puzzle_input.txt");
+    one::run();
+}
 
-    let content = read(puzzle_input_filepath).unwrap();
-    let str_content = String::from_utf8(content).unwrap();
+mod one {
+    use std::fs::read;
+    use std::path::Path;
 
-    let mut result:u32 = 0;
+    pub fn run() {
+        let puzzle_input_filepath = Path::new("assets/puzzle_input.txt");
 
-    for s in str_content.split('\n') {
-        println!("{:?}", s);
+        let content = read(puzzle_input_filepath).unwrap();
+        let str_content = String::from_utf8(content).unwrap();
 
-        let mut v: Vec<String> = Vec::new();
-        v.push(String::from(""));
+        let mut result:u32 = 0;
 
-        for x in s.chars() {
-            match x {
-                '0'..='9' => v.push(String::from(x)),
-                _ => {}
+        for s in str_content.split('\n') {
+            println!("{:?}", s);
+
+            let mut v: Vec<String> = Vec::new();
+            v.push(String::from(""));
+
+            for x in s.chars() {
+                match x {
+                    '0'..='9' => v.push(String::from(x)),
+                    _ => {}
+                }
             }
+
+            v = v.into_iter().filter(|x| x.len() > 0).collect();
+
+            println!("{:?}", v);
+
+            result += String::from(format!("{}{}", v.first().unwrap(), v.last().unwrap()))
+                .parse::<u32>().unwrap();
         }
 
-        v = v.into_iter().filter(|x| x.len() > 0).collect();
-
-        println!("{:?}", v);
-
-        result += String::from(format!("{}{}", v.first().unwrap(), v.last().unwrap()))
-            .parse::<u32>().unwrap();
+        println!("{:?}", result);
     }
-
-    println!("{:?}", result);
 }
