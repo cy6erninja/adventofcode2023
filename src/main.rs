d59ef4f9e5743f3b5e70a94df9cc3e8c08f821bc
cy6erninja <cy6erninja@gmail.com>
2024-03-25 02:57:10 +0100

    Working solution for 2.1.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,154 @@
+use std::collections::vec_deque::Iter;
+use std::collections::VecDeque;
+use std::fmt::{Debug, Formatter};
+use std::fs::read;
+use std::path::Path;
+use regex::{Captures, Regex};
+
 fn main() {
-    two::run();
+    day2_cube_conundrum_part1();
+}
+
+fn day2_cube_conundrum_part1() {
+    struct CubeSubset {
+        red: u32,
+        green: u32,
+        blue: u32
+    }
+
+    type Bag = CubeSubset;
+
+    struct Game {
+        id: u32,
+        subsets: Vec<CubeSubset>
+    }
+
+    impl Game {
+        fn is_possible(&self, bag: &Bag) -> bool {
+            for subset in &self.subsets {
+                if bag.red < subset.red || bag.green < subset.green || bag.blue < subset.blue {
+                    return false;
+                }
+            }
+
+            true
+        }
+    }
+
+    struct Games {
+        items: VecDeque<Game>
+    }
+
+    impl Games {
+        fn new() -> Self {
+            Games {
+                items: VecDeque::new()
+            }
+        }
+    }
+
+    impl Iterator for Games {
+        type Item = Game;
+
+        fn next(&mut self) -> Option<Self::Item> {
+           self.items.pop_front()
+        }
+    }
+
+    impl TryFrom<String> for Games {
+        type Error = ();
+
+        fn try_from(value: String) -> Result<Self, Self::Error> {
+
+            let mut games = VecDeque::new();
+
+             for line in value.split('\n') {
+                 let mut split_by_coloh = line.split(':');
+                 let id_re = Regex::new(r"Game (\d+)").unwrap();
+                 let Some(id) = id_re.captures(&split_by_coloh.next().unwrap())
+                 // Here we need to return an error.
+                     else {panic!("Can not parse game id.")};
+
+                 let mut subsets: Vec<CubeSubset> = Vec::new();
+
+                 for raw_subset in &mut split_by_coloh.next().unwrap().split(';').into_iter() {
+                     let red_re = Regex::new(r"(\d+) red").unwrap();
+                     let green_re = Regex::new(r"(\d+) green").unwrap();
+                     let blue_re = Regex::new(r"(\d+) blue").unwrap();
+
+                     let red = match red_re.captures(raw_subset) {
+                         Some(red) =>  *&red[1].parse::<u32>().unwrap_or(0),
+                         None => 0,
+                     };
+
+                     let green = match green_re.captures(raw_subset) {
+                         Some(green) =>  *&green[1].parse::<u32>().unwrap_or(0),
+                         None => 0,
+                     };
+
+                     let blue = match blue_re.captures(raw_subset) {
+                         Some(blue) =>  *&blue[1].parse::<u32>().unwrap_or(0),
+                         None => 0,
+                     };
+
+                     subsets.push(
+                         CubeSubset {
+                             red,
+                             green,
+                             blue,
+                         }
+                     );
+                 }
+
+                 games.push_back(Game {id: *&id[1].parse::<u32>().unwrap() , subsets });
+             }
+
+            Ok(Games { items: games })
+        }
+    }
+
+    impl Debug for Games {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            for game in &self.items {
+                println!("ID: {}", &game.id);
+                for subset in &game.subsets {
+                    println!("Red: {}; Green: {}; Blue: {}", subset.red, subset.green, subset.blue);
+                }
+            }
+
+            Ok(())
+        }
+    }
+
+    let raw_games = read_file("assets/puzzle2.txt");
+    let games = Games::try_from(raw_games).unwrap();
+    let bag = Bag {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+    // println!("{:?}", games);
+    let possible_games = games.filter(|game| game.is_possible(&bag));
+
+    let id_sum: u32 = possible_games.fold(0, |acc, game| acc + game.id);
+    // Print final sum of relevant game ids.
+    println!("{}", id_sum);
+}
+
+fn day1_trebuchet_part2() {
+
+}
+
+fn day1_trebuchet_part1() {
+
+}
+
+fn read_file(filepath: &str) -> String {
+    let puzzle_input_filepath = Path::new(filepath);
+    let content = read(puzzle_input_filepath).unwrap();
+    let str_content = String::from_utf8(content).unwrap();
+
+    str_content
 }
 
 mod two {
@@ -14,7 +163,7 @@ mod two {
     }
 
     fn read_file(filepath: &str) -> String {
-        let puzzle_input_filepath = Path::new("assets/puzzle_input.txt");
+        let puzzle_input_filepath = Path::new(filepath);
         let content = read(puzzle_input_filepath).unwrap();
         let str_content = String::from_utf8(content).unwrap();
 
